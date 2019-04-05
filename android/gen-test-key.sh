69c90d163666b9bf795381ee13dfed39b3bd3168
靳兆鲁 <1756404649@qq.com>
2019-04-05 21:35:24 +0800

    Add

--- android/gen-test-key.sh
+++ android/gen-test-key.sh
@@ -3,5 +3,5 @@ LOCAL_PATH=$(cd `dirname $0`; pwd)
 cd $LOCAL_PATH
 
 #password 666666
-keytool -genkey -v -keystore ~/key.jks -keyalg RSA -keysize 2048 -validity 10000 -alias key
+keytool -genkey -v -keystore ./key.jks -keyalg RSA -keysize 2048 -validity 10000 -alias key
 # keytool -importkeystore -srckeystore ./test.keystore -destkeystore ./test.keystore -deststoretype pkcs12
